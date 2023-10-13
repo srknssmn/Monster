c95753733c8660eee8d1798d877ae0a8209c3b78
srknssmn <srknssmn@gmail.com>
2023-10-14 01:37:57 +0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from 'dotenv';
-import session from 'express-session';
 
 const app = express();
 dotenv.config();
@@ -14,16 +13,6 @@ global.walletIN = null
 // Middlewares
 app.use(express.static('public'))
 app.use(express.json());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {}
-}))
-app.use('*', (req,res, next) => {
-  walletIN = req.session.walletID;
-  next();
-})
 
 import pageRoute from './routes/pageRoute.js';
 import walletRoute from './routes/walletRoute.js';
--- controllers/walletController.js
+++ controllers/walletController.js
@@ -1,11 +1,7 @@
-// import {recoverPersonalSignature} from '@metamask/eth-sig-util'
-// import {bufferToHex} from 'ethereumjs-util';
-
 const walletConnect = async (req, res) => {
     
     try {
         const {walletAddress} = req.query;
-        req.session.walletID = await walletAddress
         res.redirect('/')
     } catch (error) {
         res.status(400).json({
@@ -14,25 +10,6 @@ const walletConnect = async (req, res) => {
         })
     }
 }
-
-// const walletVerify = async (req, res) => {
-    
-//     const { walletAddress, signedNonce } = req.query;
-//     const nonce = nonceList[walletAddress];
-    
-//     try {
-
-//         const hexNonce = bufferToHex(Buffer.from(nonce, "utf8"));
-//         const retrievedAddress = recoverPersonalSignature({
-//             data: hexNonce,
-//             signature: signedNonce,
-//         });
-
-//     } catch (error) {
-//         console.log(error)
-//         res.status(400).json(error)
-//     }
-// }
 
 export {
     walletConnect
