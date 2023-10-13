76511214818124be00cbba24b9613d74192b6c8f
srknssmn <srknssmn@gmail.com>
2023-10-14 02:34:54 +0300

    update

--- public/js/userArray.js
+++ public/js/userArray.js
@@ -3,6 +3,7 @@ import { MONSTER_ABI } from "/constants/abi.js";
 
 let userList = document.querySelector('#userList')
 let kickersList = document.querySelector('#kickersList')
+let userListSect = document.querySelector('#userListSect')
 
 kickersList.addEventListener('click' , userArray)
 
@@ -13,21 +14,29 @@ async function userArray() {
     
     const userArray = await contract.showKickersArray();
     kickersList.hidden = true
+    userListSect.hidden = false
 
-    for (let i = 0 ; i < userArray.length ; i++ ) {
-        const listItem = document.createElement("li");
-        const div =  document.createElement("div");
-        div.classList.add("d-flex" , "flex-row")
-        listItem.appendChild(div);
-        const kicker = document.createElement("p");
-        const space = document.createElement("p");
-        const kickerCount = document.createElement("p");
-        div.appendChild(kicker);
-        div.appendChild(space);
-        div.appendChild(kickerCount);
-        kicker.innerHTML = userArray[i].kicker
-        space.innerHTML = "&nbsp = &nbsp"
-        kickerCount.innerHTML = userArray[i].kickNumber
-        userList.appendChild(listItem);
+    let newUserArray = await [...userArray]
+    let sortedUserArray = await newUserArray.sort((a, b) => ethers.utils.formatEther(b.kickNumber) - ethers.utils.formatEther(a.kickNumber));
+
+    for (let i = 0 ; i < sortedUserArray.length ; i++ ) {
+        if (i < 10) {
+            const listItem = document.createElement("li");
+            const div =  document.createElement("div");
+            div.classList.add("d-flex" , "flex-row")
+            listItem.appendChild(div);
+            const kicker = document.createElement("p");
+            const space = document.createElement("p");
+            const kickerCount = document.createElement("p");
+            div.appendChild(kicker);
+            div.appendChild(space);
+            div.appendChild(kickerCount);
+            let first = sortedUserArray[i].kicker.slice(0, 5)
+            let last = sortedUserArray[i].kicker.slice(-5)
+            kicker.innerHTML = first + "..." + last
+            space.innerHTML = "&nbsp = &nbsp"
+            kickerCount.innerHTML = sortedUserArray[i].kickNumber
+            userList.appendChild(listItem);
+        }
     }
 };
