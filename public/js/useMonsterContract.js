c5b675e2df1f18c9226ab5deda2567b5d5ef796d
srknssmn <srknssmn@gmail.com>
2023-10-14 14:01:52 +0300

    update

--- public/js/useMonsterContract.js
+++ public/js/useMonsterContract.js
@@ -1,6 +1,13 @@
 import { MONSTER_ADDRESS } from "/constants/address.js";
 import { MONSTER_ABI } from "/constants/abi.js";
 import {connectWalletfunc} from "/js/connectWallet.js";
+import {verifyNetwork} from "/js/verifyNetwork.js";
+
+let ouchAudio = document.querySelector('#ouchAudio')
+
+function playAudio() {
+    ouchAudio.play(); 
+}
 
 document.querySelector("#kickMonster").addEventListener('click' , kickMonsterfunc)
 
@@ -10,6 +17,8 @@ async function kickMonsterfunc(event) {
     // get the wallet address from metamask
     const accounts = await ethereum.request({method: 'eth_accounts'});
     if (accounts.length) {
+        await verifyNetwork();
+        
         let walletAddress;
         const provider = await new ethers.providers.Web3Provider(window.ethereum);
         // await provider.send("eth_requestAccounts", [])
@@ -17,6 +26,7 @@ async function kickMonsterfunc(event) {
         const contract = await new ethers.Contract(MONSTER_ADDRESS, MONSTER_ABI, signer);
     
         const txn = await contract.kickMonster();
+        await playAudio();
         await txn.wait();
         await console.log("success")
 
