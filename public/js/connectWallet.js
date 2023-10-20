23f00a048e6c095f8729d99a98454f73c646837d
srknssmn <srknssmn@gmail.com>
2023-10-20 19:29:57 +0300

    update

--- public/js/connectWallet.js
+++ public/js/connectWallet.js
@@ -5,6 +5,8 @@ async function connectWalletfunc() {
 
     if (!window.ethereum) {
         console.log("MetaMask not installed; using read-only defaults")
+        window.alert("Install Metamask")
+        if(confirm("Metamask Download")) document.location = 'https://metamask.io/download/';
     }
     await verifyNetwork();
     
